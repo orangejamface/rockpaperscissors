666cf59460835c8db89cd4c580077f72892a8c21
eddybayes <orangejamface@gmail.com>
2022-04-18 15:53:18 -0400

    added buttons, linked first button to function so that it takes the player selection of the button text and rthat is used as the playerselection argument of the playRound function

--- java.js
+++ java.js
@@ -9,12 +9,21 @@
     return num;
 }
 
+const btn = document.querySelector('.rock');
+// const btn = document.querySelectorAll('button');
+btn.addEventListener('click', playRound);
+// btn.forEach(btn => btn.addEventListener ('click', playRound));  
+
+
 function playRound() {
 
-    let result = 'default';
+    // let result = 'default';
 
-    let input = (prompt('ROCK, PAPER, SCISSORS?', ''));
-    let playerSelection = input.toUpperCase();
+    // let input = (prompt('ROCK, PAPER, SCISSORS?', ''));
+    // let playerSelection = input.toUpperCase();
+    // let playerSelection = 'ROCK';
+    let playerSelection = btn.textContent;
+
     let computerPlay = numGen();
     if (computerPlay === 1) {
             computerSelection = 'ROCK'
@@ -48,10 +57,10 @@ function playRound() {
     console.log(`Computer: ${cScore} , Player:  ${pScore}`);
 }
 
-function game() {
-     for (let i = 0; i < 5; i++) {
-       playRound();
-     } 
-}
+// function game() {
+//      for (let i = 0; i < 5; i++) {
+//        playRound();
+//      } 
+// }
 
-game();
+// game();
